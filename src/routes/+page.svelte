9012b36b7cad762c9c7263fc41d6b8fef6d93441
Binayak Dasgupta <binayakd86@gmail.com>
2023-03-04 22:27:23 +0800

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+  import Layout from './__layout.svelte';
+</script>
+
+<svelte:head>
+<title>My Blog - Home</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
