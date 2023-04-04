86e685a68d30116c819a0ccdcbad6166c4ca4916
Binayak <binayakd86@gmail.com>
2023-04-04 23:13:39 +0800

    working posts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -13,7 +13,6 @@ export interface PostData {
   slug: string;
   title: string;
   date: string;
-  excerpt: string;
   content: string;
 }
 
@@ -27,72 +26,9 @@ export async function getPostDataBySlug(slug: string){
     slug: slug,
     title: title,
     date: date,
-    excerpt: content.split('\n')[0],
     content: content
   }
 
   return postData
 
 }
-
-export function getPosts(dir: string): PostData[] {
-  const files = fs.readdirSync(dir);
-  const postsDataList: PostData[] = [];
-
-  files.forEach((file) => {
-    const filePath = path.join(dir, file);
-    const fileStat = fs.statSync(filePath);
-
-    if (fileStat.isDirectory()) {
-      const subfolderPostsData = getPosts(filePath);
-      postsDataList.push(...subfolderPostsData);
-    }
-
-    if (path.extname(filePath) === '.md') {
-      const slug = path.basename(filePath).split(".")[0]
-      const postData = getPostBySlug(slug)
-      postsDataList.push(postData)
-    }
-  });
-
-  return postsDataList;
-}
-
-export const getPostBySlug = (slug: string): PostData =>{
-
-  console.log(`Getting slug: ${slug}`)
-  const year = slug.split("-")[0]
-  const filePath = `posts/${year}/${slug}.md`
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const { data, content } = matter(fileContents);
-
-  const postData: PostData = {
-    slug: slug,
-    title: data.title,
-    date: data.date,
-    excerpt: content.split('\n')[0],
-    content: content
-
-  }
-
-  return postData
-};
-
-export const parseMarkdown = (text: string): string => {
-  marked.setOptions({
-    renderer: new marked.Renderer(),
-    highlight: function (code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(code, { language }).value;
-    },
-    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
-    pedantic: false,
-    gfm: true,
-    breaks: false,
-    sanitize: false,
-    smartypants: false,
-    xhtml: false
-  });
-
-  return DOMPurify.sanitize(marked.parse(text));
-};
--- src/routes/posts/[slug]/+page.ts
+++ src/routes/posts/[slug]/+page.ts
@@ -1,36 +1,10 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-import { getPostBySlug } from '$lib/posts';
+import { getPostDataBySlug } from '$lib/posts';
 
  
-// export const load = (({ params }) => {
-
-//   const postData = getPostBySlug(params.slug)
-
-//   return {
-//     title: postData.title,
-//     date: postData.date,
-//     content: postData.content
-//   };
-//   throw error(404, 'Not found');
-// }) satisfies PageLoad;
-
-
-// export async function load({ params }){
-//   const year = params.slug.split("-")[0]
-//   const post = await import(`../../../../posts/${year}/${params.slug}.md`)
-//   const { title, date } = post.metadata
-//   const content = post.default
-
-//   return {
-//     content,
-//     title,
-//     date,
-//   }
-// }
-
 export async function load({ params }){
-  const postData = getPostBySlug(params.slug)
+  const postData = await getPostDataBySlug(params.slug)
 
   return {
     title: postData.title,
